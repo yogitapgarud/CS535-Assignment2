bfea38f25f923b437d98d45ae8ad842fb6d44c67
Yogita Garud <garudyogitap@gmail.com>
2018-02-16 19:43:01 -0800

    added forward and train

--- MLP_Skeleton.py
+++ MLP_Skeleton.py
@@ -16,27 +16,11 @@ import numpy as np
 class LinearTransform(object):
 
     def __init__(self, W, b):
-        W = W.transpose()
+        self.W = W.transpose()
 
     def forward(self, x):
-	l = np.dot(W, self.x + b)
-
-    def backward(
-        self, 
-        grad_output, 
-        learning_rate=0.5, 
-        momentum=0.0, 
-        l2_penalty=0.0,
-    ):
-	# DEFINE backward function
-# ADD other operations in LinearTransform if needed
-
-# This is a class for a ReLU layer max(x,0)
-class ReLU(object):
-
-    def forward(self, x):
-	# DEFINE forward function
-        z2 = max(x, 0)
+        l = x.dot(self.W)
+        return l
 
     def backward(
         self, 
@@ -45,35 +29,63 @@ class ReLU(object):
         momentum=0.0, 
         l2_penalty=0.0,
     ):
+        return X
+	# DEFINE backward function
+# ADD other operations in LinearTransform if needed
+
+# This is a class for a ReLU layer max(x,0)
+class ReLU(object):
+
+    def forward(self, x):
+	# DEFINE forward function
+        x[x<0] = 0
+        return x 
+
+    def backward(
+        self, 
+        grad_output, 
+        learning_rate=0.0, 
+        momentum=0.0, 
+        l2_penalty=0.0,
+    ):
+        
     # DEFINE backward function
 # ADD other operations in ReLU if needed
 
 # This is a class for a sigmoid layer followed by a cross entropy layer, the reason 
 # this is put into a single layer is because it has a simple gradient form
 class SigmoidCrossEntropy(object):
+
+        def __init__(self, w):
+            self.w = w
+
 	def forward(self, x):
-            sigmoid(z)
+            z = np.dot(x, self.w)
+            return sigmoid(z)
 
 	def backward(
-	    self, 
-	    grad_output, 
-		learning_rate=0.0,
-		momentum=0.0,
-		l2_penalty=0.0
+            self, 
+            grad_output, 
+            learning_rate=0.0,
+            momentum=0.0,
+            l2_penalty=0.0
 	):
+
 		# DEFINE backward function
 # ADD other operations and data entries in SigmoidCrossEntropy if needed
 
         def sigmoid(z):
-            return 1/(1 + numpy.exp(-z)) 
+            return 1/(1 + np.exp(-z)) 
 
 # This is a class for the Multilayer perceptron
 class MLP(object):
 
     def __init__(self, input_dims, hidden_units):
     # INSERT CODE for initializing the network
-        W = np.random.rand(input_dims, hidden_units)
-        b = np.random.rand(hidden_units, 1)
+        self.W1 = np.random.rand(hidden_units, input_dims)
+        self.W2 = np.random.rand(hidden_units, 1)
+        self.momentumw1 = np.zeros(hidden_units, input_dims)
+        self.momentumw2 = np.zeros(hidden_units, 1)
 
     def train(
         self, 
@@ -84,6 +96,19 @@ class MLP(object):
         l2_penalty,
     ):
 	# INSERT CODE for training the network
+        lt = LinearTransform(self.W1)
+        relu = ReLU()
+        sig = SigmoidCrossEntropy(self.W2)
+        z1 = lt.forward(X)
+        a1 = relu.forward(z1)
+        z2 = sig.forward(a1)
+        
+        dw1 = sig.backward() * relu.backward() * lt.backward()
+        dw2 = sig.backward()
+
+        loss = np.dot(y_batch, np.log(z2)) + np.dot((1-y), np.log(1-z2))
+        print "loss: ", loss.shape()
+        return dw1, dw2, loss
 
     def evaluate(self, x, y):
 	# INSERT CODE for testing the network
@@ -99,23 +124,38 @@ if __name__ == '__main__':
     test_y = data['test_labels']
 	
     num_examples, input_dims = train_x.shape
-    hidden_units = 2 * input_dims[1]
+    hidden_units = 10
 	# INSERT YOUR CODE HERE
 	# YOU CAN CHANGE num_epochs AND num_batches TO YOUR DESIRED VALUES
-	num_epochs = 10
-	num_batches = 1000
+    num_epochs = 10
+    num_batches = 10
     mlp = MLP(input_dims, hidden_units)
 
     train_loss = 0
-
+    total_loss = 0
+    batch_size = num_examples / num_batches
+    u = 0.6
+    
     for epoch in xrange(num_epochs):
 
 	# INSERT YOUR CODE FOR EACH EPOCH HERE
+        np.random.shuffle(train_x)
 
         for b in xrange(num_batches):
-			total_loss = 0.0
+      	    total_loss = 0.0
 			# INSERT YOUR CODE FOR EACH MINI_BATCH HERE
 			# MAKE SURE TO UPDATE total_loss
+            batch_size = num_examples/num_batches
+            i = b * batch_size
+
+            dw1 , dw2 = mlp.train(x[i:i+batch_size,:],y[i:i+batch_size], learning_rate, momentumw1, momentumw2, penalty)
+                        
+            mlp.momentumw1 = u * mlp.momentumw1 - learning_rate * dw1                                             
+            mlp.momentumw2 = u * mlp.momentumw2 - learning_rate * dw2
+
+            mlp.W1 = mlp.W1 + mlp.momentumw1
+            mlp.W2 = mlp.W2 + mlp.momentumw2
+
             print(
                 '\r[Epoch {}, mb {}]    Avg.Loss = {:.3f}'.format(
                     epoch + 1,
